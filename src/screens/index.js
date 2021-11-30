a12109526ac62212838a62d8f5e2b5cedcb9decf
jepzekiel <jezedevkiel21@gmail.com>
2021-11-30 17:46:21 +0800

    Update index.js

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,3 +1,4 @@
 export {default as Splash} from './Splash';
 export {default as Login} from './Login';
 export {default as Onboarding} from './Onboarding';
+export {default as Profile } from './Profile';
