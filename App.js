9480b312a563f8e8e88e269fcc467c7a4168e398
jepzekiel <jezedevkiel21@gmail.com>
2021-11-30 17:47:38 +0800

    Update App.js

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {View,Text} from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import {Splash,Onboarding,Login} from './src/screens'
+import {Splash,Onboarding,Login,Register,Profile} from './src/screens'
 
 const Stack = createNativeStackNavigator();
 const App = ()=>{
@@ -13,6 +13,8 @@ const App = ()=>{
               <Stack.Screen name="Splash" component={Splash} />
               <Stack.Screen name="Onboarding" component={Onboarding} />
               <Stack.Screen name="Login" component={Login} />
+                <Stack.Screen name="Register" component={Register} />
+                  <Stack.Screen name="Profile" component={Profile} />
             </Stack.Navigator>
           </NavigationContainer>
   );
